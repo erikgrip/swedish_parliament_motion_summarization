c1576c9733edfac1c619c758e8a30be63e8a0e7e
Erik Grip <erik.grip@outlook.com>
2022-07-16 15:26:41 +0200

    More work on getting t5 lit_model to work.

--- text_summarizer/lit_models/t5.py
+++ text_summarizer/lit_models/t5.py
@@ -1,24 +1,26 @@
-import pytorch_lightning as pl
-from transformers import T5ForConditionalGeneration
+import argparse
+
+from text_summarizer.lit_models import BaseLitModel
+from text_summarizer.models.t5 import t5
 
 
-class t5LitModel(pl.LightningDataModule):
-    def __init__(self):
-        super().__init__()
-        self.model = T5ForConditionalGeneration("t5-base", return_dict=True)
+class t5LitModel(BaseLitModel):
+    def __init__(self, model, args: argparse.Namespace = None):
+        super().__init__(model, args=args)
+        self.model = model
 
     def forward(self, input_ids, attention_mask, decoder_attention_mask, labels=None):
-        output = self.model(
+        loss, logits = self.model(
             input_ids,
             attention_mask=attention_mask,
             labels=labels,
             decoder_attention_mask=decoder_attention_mask,
         )
-        return output.loss, output.logits
+        return loss, logits
 
     def training_step(self, batch, batch_idx):
         input_ids = batch["text_input_ids"]
-        attention_mask = batch["attention_mask"]
+        attention_mask = batch["text_attention_mask"]
         labels = batch["labels"]
         labels_attention_mask = batch["labels_attention_mask"]
 
@@ -26,7 +28,7 @@ class t5LitModel(pl.LightningDataModule):
             input_ids=input_ids,
             attention_mask=attention_mask,
             labels=labels,
-            labels_attention_mask=labels_attention_mask,
+            decoder_attention_mask=labels_attention_mask,
         )
 
         self.log("train_loss:", loss, prog_bar=True, logger=True)
@@ -34,7 +36,7 @@ class t5LitModel(pl.LightningDataModule):
 
     def validation_step(self, batch, batch_idx):
         input_ids = batch["text_input_ids"]
-        attention_mask = batch["attention_mask"]
+        attention_mask = batch["text_attention_mask"]
         labels = batch["labels"]
         labels_attention_mask = batch["labels_attention_mask"]
 
@@ -42,7 +44,7 @@ class t5LitModel(pl.LightningDataModule):
             input_ids=input_ids,
             attention_mask=attention_mask,
             labels=labels,
-            labels_attention_mask=labels_attention_mask,
+            decoder_attention_mask=labels_attention_mask,
         )
 
         self.log("val_loss:", loss, prog_bar=True, logger=True)
@@ -50,7 +52,7 @@ class t5LitModel(pl.LightningDataModule):
 
     def test_step(self, batch, batch_idx):
         input_ids = batch["text_input_ids"]
-        attention_mask = batch["attention_mask"]
+        attention_mask = batch["text_attention_mask"]
         labels = batch["labels"]
         labels_attention_mask = batch["labels_attention_mask"]
 
@@ -58,7 +60,7 @@ class t5LitModel(pl.LightningDataModule):
             input_ids=input_ids,
             attention_mask=attention_mask,
             labels=labels,
-            labels_attention_mask=labels_attention_mask,
+            decoder_attention_mask=labels_attention_mask,
         )
 
         self.log("test_loss:", loss, prog_bar=True, logger=True)
