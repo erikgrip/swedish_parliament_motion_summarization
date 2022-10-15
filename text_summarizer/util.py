ecb518a76d6bd9bc0fa1355e05f30df5dbf379b3
Erik Grip <erik.grip@outlook.com>
2022-10-15 21:09:06 +0200

    Add function to make predictions

--- text_summarizer/util.py
+++ text_summarizer/util.py
@@ -26,3 +26,35 @@ def download_url(url, filename):
     """Download a file from url to filename, with a progress bar."""
     with TqdmUpTo(unit="B", unit_scale=True, unit_divisor=1024, miniters=1) as t:
         urlretrieve(url, filename, reporthook=t.update_to, data=None)  # nosec
+
+
+def summarize(model, text, tokenizer, text_max_num_tokens, summary_max_num_tokens):
+    text_encoding = tokenizer(
+        text,
+        max_length=text_max_num_tokens,
+        padding="max_length",
+        truncation=True,
+        return_attention_mask=True,
+        add_special_tokens=True,
+        return_tensors="pt",
+    )
+    print(text_encoding)
+
+    generated_ids = model.model.generate(
+        input_ids=text_encoding["input_ids"],
+        attention_mask=text_encoding["attention_mask"],
+        max_length=summary_max_num_tokens,
+        num_beams=2,
+        repetition_penalty=2.5,
+        length_penalty=1.0,
+        early_stopping=True,
+    )
+
+    preds = [
+        tokenizer.decode(
+            gen_id, skip_special_tokens=True, clean_up_tokenization_spaces=True
+        )
+        for gen_id in generated_ids
+    ]
+
+    return "".join(preds)
