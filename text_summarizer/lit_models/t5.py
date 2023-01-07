1fd89cfa2d8feac9baa08730b06d0890e1ff7103
Erik Grip <erik.grip@outlook.com>
2023-01-07 10:29:28 +0100

    Move encoding from model/litmodel

--- text_summarizer/lit_models/t5.py
+++ text_summarizer/lit_models/t5.py
@@ -1,11 +1,6 @@
 import argparse
 
-
 from text_summarizer.lit_models.base import BaseLitModel
-
-MAX_TEXT_TOKENS = 512
-MAX_SUMMARY_TOKENS = 64
-TOKENIZER = MT5Tokenizer.from_pretrained("google/mt5-small")
 
 
 class T5LitModel(BaseLitModel):  # pylint: disable=too-many-ancestors
@@ -14,9 +9,6 @@ class T5LitModel(BaseLitModel):  # pylint: disable=too-many-ancestors
     def __init__(self, model, args: argparse.Namespace = None):
         super().__init__(model, args=args)
         self.model = model
-        print(args)
-        import sys
-        sys.exit(0)
 
     def forward(self, input_ids, attention_mask, decoder_attention_mask, labels=None):
         """Forward pass through self.model."""
@@ -29,49 +21,30 @@ class T5LitModel(BaseLitModel):  # pylint: disable=too-many-ancestors
         return loss, logits
 
     def training_step(self, batch, batch_idx):
-        print(batch["text"])
-        print(batch["label"])
-
-        print(len(batch))
-        encoded_texts = [self.encode(t, self.max_text_tokens) for t in batch["text"]]
-        encoded_labels = [
-            self.encode(l, self.max_summary_tokens, set_zero_to=-100)
-            for l in batch["label"]
-        ]
-        input_ids = [e["text_input_ids"] for e in encoded_texts]
-        attention_mask = [e["text_attention_mask"] for e in encoded_texts]
-        labels = [e["text_input_ids"] for e in encoded_labels]
-        decoder_attention_mask = [e["text_attention_mask"] for e in encoded_labels]
-
         loss, _ = self(
-            input_ids=input_ids,
-            attention_mask=attention_mask,
-            labels=labels,
-            decoder_attention_mask=decoder_attention_mask,
+            input_ids=batch["text_input_ids"],
+            attention_mask=batch["text_attention_mask"],
+            labels=batch["text_input_ids"],
+            decoder_attention_mask=batch["text_attention_mask"],
         )
         self.log("train_loss:", loss, prog_bar=True, logger=True)
         return loss
 
     def validation_step(self, batch, batch_idx):
-        encoded_texts = [self.encode(t) for t in batch["text"]]
-        encoded_labels = [self.encode(l, set_zero_to=-100) for l in batch["labels"]]
-
         loss, _ = self(
-            input_ids=encoded_texts["text_input_ids"],
-            attention_mask=encoded_texts["text_attention_mask"],
-            labels=encoded_labels["text_input_ids"],
-            decoder_attention_mask=encoded_labels["text_attention_mask"],
+            input_ids=batch["text_input_ids"],
+            attention_mask=batch["text_attention_mask"],
+            labels=batch["text_input_ids"],
+            decoder_attention_mask=batch["text_attention_mask"],
         )
         self.log("val_loss:", loss, prog_bar=True, logger=True)
-        self.log("sample prediction", {"text": encoded_texts["text_input_ids"]})
+        self.log("sample prediction", {"text": batch["text_input_ids"]})
 
     def test_step(self, batch, batch_idx):
-        encoded_texts = [self.encode(t) for t in batch["text"]]
-        encoded_labels = [self.encode(l, set_zero_to=-100) for l in batch["labels"]]
         loss, _ = self(
-            input_ids=encoded_texts["text_input_ids"],
-            attention_mask=encoded_texts["text_attention_mask"],
-            labels=encoded_labels["text_input_ids"],
-            decoder_attention_mask=encoded_labels["text_attention_mask"],
+            input_ids=batch["text_input_ids"],
+            attention_mask=batch["text_attention_mask"],
+            labels=batch["text_input_ids"],
+            decoder_attention_mask=batch["text_attention_mask"],
         )
         self.log("test_loss:", loss, prog_bar=True, logger=True)
--- text_summarizer/models/t5.py
+++ text_summarizer/models/t5.py
@@ -3,12 +3,9 @@ import argparse
 
 from torch import nn
 from transformers.models.mt5.modeling_mt5 import MT5ForConditionalGeneration
-from transformers.models.mt5 import MT5Tokenizer
+
 
 MT5_VERSION = "small"
-MAX_TEXT_TOKENS = 512
-MAX_TITLE_TOKENS = 64
-
 
 class MT5(nn.Module):
     """Model class for MT5 models for conditional generation."""
@@ -22,9 +19,6 @@ class MT5(nn.Module):
         self.model = MT5ForConditionalGeneration.from_pretrained(
             model, return_dict=True
         )
-        self.tokenizer = MT5Tokenizer.from_pretrained(model)
-        self.max_text_tokens = self.args.get("max_text_tokens", MAX_TEXT_TOKENS)
-        self.max_title_tokens = self.args.get("max_title_tokens", MAX_TITLE_TOKENS)
 
     @staticmethod
     def add_to_argparse(parser):  # pylint: disable=missing-function-docstring
@@ -33,18 +27,6 @@ class MT5(nn.Module):
             type=str,
             default=MT5_VERSION,
             help="MT5 model version (small, base, large, xl, xxl)",
-        )
-        parser.add_argument(
-            "--max_text_tokens",
-            type=int,
-            default=MAX_TEXT_TOKENS,
-            help="Maximum number of tokens to use from text",
-        )
-        parser.add_argument(
-            "--max_title_tokens",
-            type=int,
-            default=MAX_TITLE_TOKENS,
-            help="Maximum number of tokens to generate for motion title",
         )
         return parser
 
