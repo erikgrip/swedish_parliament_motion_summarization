df6852f5573299d49af3f559298b8d921aa94af2
Erik Grip <erik.grip@outlook.com>
2023-02-05 19:43:53 +0100

    Don't use overfit batch in test-run.

--- training/tests/test_run_experiment.sh
+++ training/tests/test_run_experiment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 FAILURE=false
 
-PYTHONPATH=. python training/run_experiment.py --max_epochs=3 --batch_size=1 --num_workers=4 --data_class=MotionsDataModule --model_class=MT5 --overfit_batches=1 || FAILURE=true
+PYTHONPATH=. python training/run_experiment.py --max_epochs=1 --gpus=0 --data_fraction=0.0001 || FAILURE=true
 
 if [ "$FAILURE" = true ]; then
   echo "Test for run_experiment.py failed"
