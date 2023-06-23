839b9f900032d49082499a793cc269d6954d6d50
Erik Grip <erik.grip@outlook.com>
2023-06-23 22:40:43 +0200

    Use yaml.safe_load() after security warning.

--- motion_title_generator/save_checkpoint_to_huggingface.py
+++ motion_title_generator/save_checkpoint_to_huggingface.py
@@ -41,7 +41,7 @@ def load_litmodel_from_checkpoint(checkpoint_path, cfg_path):
     logging.info("Loading checkpoint config %s ...", cfg_path)
     with open(cfg_path, "r", encoding="utf-8") as hparams_file:
         lightning_config = argparse.Namespace(
-            **yaml.load(hparams_file, Loader=yaml.Loader)
+            **yaml.safe_load(hparams_file)
         )
 
     model = t5.MT5(data_config={}, args=lightning_config)
