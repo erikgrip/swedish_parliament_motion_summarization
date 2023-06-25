a394dc982591b4b384511dc5e98b821b6340034f
Erik Grip <erik.grip@outlook.com>
2023-06-25 14:37:48 +0200

    Linting and cleaning up.

--- motion_title_generator/lit_models/t5.py
+++ motion_title_generator/lit_models/t5.py
@@ -3,10 +3,7 @@ from typing import Dict, List, Optional
 
 from transformers.models.mt5 import MT5Tokenizer
 
-from motion_title_generator.data.t5_encodings_dataset import (
-    MAX_TEXT_TOKENS,
-    MAX_TITLE_TOKENS,
-)
+from motion_title_generator.data.t5_encodings_dataset import MAX_TITLE_TOKENS
 from motion_title_generator.lit_models.base import BaseLitModel
 from utils.encode_decode import generate
 
@@ -19,25 +16,7 @@ class MT5LitModel(BaseLitModel):  # pylint: disable=too-many-ancestors
         self.args = args if args is not None else {}
         self.model = model
         self.tokenizer = MT5Tokenizer.from_pretrained(model.model_name)
-        self.max_text_tokens = self.args.get("max_title_tokens", MAX_TEXT_TOKENS)
-        self.max_title_tokens = self.args.get("max_title_tokens", MAX_TITLE_TOKENS)
         self.validation_step_outputs: List[Dict] = []
-
-    @staticmethod
-    def add_to_argparse(parser):  # pylint: disable=missing-function-docstring
-        parser.add_argument(
-            "--max_text_tokens",
-            type=int,
-            default=MAX_TEXT_TOKENS,
-            help="Number of tokens to use from text.",
-        )
-        parser.add_argument(
-            "--num_title_tokens",
-            type=int,
-            default=MAX_TITLE_TOKENS,
-            help="Max number of tokens to generate in summary.",
-        )
-        return parser
 
     def forward(self, input_ids, attention_mask, decoder_attention_mask, labels=None):
         """Forward pass through self.model."""
@@ -79,12 +58,11 @@ class MT5LitModel(BaseLitModel):  # pylint: disable=too-many-ancestors
         """Generate title for a sample of validation set."""
         sample_output = sample(self.validation_step_outputs, 1)[0]
         self.validation_step_outputs.clear()
-
         generated_title = generate(
             self.model.model,
             self.tokenizer,
             sample_output,
-            self.max_title_tokens,
+            self.args.get("max_title_tokens", MAX_TITLE_TOKENS),
         )
         self.logger.experiment.add_text(
             "actual title", sample_output["title"][0], global_step=self.global_step
